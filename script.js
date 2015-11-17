8c05204d43458a760c3fe40ff2155b4ab22d0eb4
adesanto <aarondesanto@gmail.com>
2015-11-17 15:43:08 -0500

    logic is finished, styles needed.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var shopApp = angular.module('shopModule', []);
 shopApp.controller('mainController', function($scope) {
-  $scope.list = [
+  $scope.itemList = [
     {
       name: 'Eggs Grade C',
       price: 1.99
@@ -14,15 +14,23 @@ shopApp.controller('mainController', function($scope) {
       price: 3.99
     },
   ];
-  $scope.thebutton = function(name, price) {
-    if ((name && price) !== (undefined)) { // If user passes data
-      $scope.list.push({
-        name: name,
-        price: price
+  $scope.getTotal = function() {
+    total = 0;
+    $scope.itemList.forEach(function(listed) {
+      total += parseFloat(listed.price);
+      $scope.total = total;
+    });
+  };
+  $scope.getTotal();
+  $scope.addItem = function() {
+    if ((nameIn && priceIn) !== (undefined)) { // If user passes data
+      $scope.itemList.push({
+        name: $scope.nameIn,
+        price: $scope.priceIn
       });
+      $scope.getTotal();
     } else { // If user submites with either field blank
       alert("Oops, looks like you left something blank.")
     }
-    return this;
   };
 });
